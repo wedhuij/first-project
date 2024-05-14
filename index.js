f9c2ff8ecaf2d617451b11048b277e50ebecb1bd
wedhuij <gs1ajsjdjdm@hotmail.com>
2024-05-14 19:59:41 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+const binarySearchIterative = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
