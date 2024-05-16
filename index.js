74cc909143310ee4a9ae962ea051f0cd3abfe2ed
wedhuij <gs1ajsjdjdm@hotmail.com>
2024-05-16 19:57:08 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+function wordBreak(s, wordDict) {
+  const wordSet = new Set(wordDict);
+  const dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+  for (let end = 1; end <= s.length; end++) {
+    for (let start = 0; start < end; start++) {
+      if (dp[start] && wordSet.has(s.substring(start, end))) {
+        dp[end] = true;
+        break;
+      }
+    }
+  }
+  return dp[s.length];
+}
